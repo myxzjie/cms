9914e6a216587ae223d386268d01b052f72ca7bb
xzjie <513961835@qq.com>
2016-11-01 20:39:39 +0800

    过滤文件

---
+++ src/main/java/com/xzjie/cms/App.java
@@ -0,0 +1,13 @@
+package com.xzjie.cms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
